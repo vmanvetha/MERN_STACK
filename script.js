0cc40c84e58e242ac3ec83cf47e144c2a654fcd4
manvetha vinnakota <manvethavinnakota@gmail.com>
2025-11-07 12:12:59 +0530

    Day 1 frontend project

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log('MY NAME IS MANVETHA AND IM 18 YEARS OLD>')
+let name = 'manvetha'
+let age = 18
+let isStudent = true
+console.log(name)
+console.log(age)
+console.log(isStudent)
+let x = 2
+let y = 5
+let z = 6
+console.log(x*y*z)
+let a = 10
+let b = 35
+console.log(a+b)
+let c =  25
+let d = 46
+[c,d] = [d,c]
+console.log(c)
+console.log(d)
+let number = 27
+console.log(number%4)
+let word = "manvetha"
+console.log(word.length)
+let num = 6
+if (num%2==0){
+    console.log('EVEN')
+}
+else{
+    console.log('ODD')
+}
+let word1 = 'manvetha'
+let word2 = 'vinnakota'
+console.log(word1+" "+word2)
+let t = "25"
+let result = Number(t) + 5
+console.log(result)
